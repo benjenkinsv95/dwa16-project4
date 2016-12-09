1b4e13629aa66051fcc6d574062b6a0f2574b808
Ben Jenkins <benjenkinsv95@gmail.com>
2016-12-09 13:26:24 -0600

    Adding some seeders

--- database/seeds/VoicesTableSeeder.php
+++ database/seeds/VoicesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Voice;
 use Illuminate\Database\Seeder;
 
 class VoicesTableSeeder extends Seeder
@@ -11,6 +12,24 @@ class VoicesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $voices = [
+            ['Josh (Child)','English (USA)'],
+            ['Will (FromAfar)','English (USA)'],
+            ['Graham','English (UK)']
+        ];
+
+        $existingVoices = Voice::all()->keyBy('name')->toArray();
+        $user_id = Author::where('email','=','benjenkinsv95@gmail.com')->pluck('id')->first();
+
+        foreach($voices as $voice) {
+
+            if(!array_key_exists($voice[0],$existingVoices)) {
+                $voice = Voice::create([
+                    'name' => $voice[0],
+                    'language' => $voice[1],
+                    'user_id' => $user_id,
+                ]);
+            }
+        }
     }
 }
