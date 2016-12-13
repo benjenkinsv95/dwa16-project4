971d176d1f0c4ec20a97d552c98f2163424fbe35
Ben Jenkins <benjenkinsv95@gmail.com>
2016-12-12 21:15:23 -0600

    Cleaning up home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,26 @@
 @section('description', 'Create better pronunciations easier.')
 
 @section('content')
-    <h2>TODO(ben): Welcome</h2>
-    <p>TODO(ben): Application description. Continually whiteboard just in time testing procedures through leveraged products. Globally integrate cutting-edge processes and alternative testing procedures. Monotonectally deploy parallel scenarios through functional niche markets.</p>
+    <h2>Welcome!</h2>
+    <p>This site is dedicated to cataloging and building phonetic pronunciations for the Acapela Group text-to-speech voices.</p>
+    <p>View, edit, and create pronunciations then try playing them back with the <a href="https://acapela-box.com/AcaBox/index.php">Acapela Speech Demo</a>.</p>
+    <br>
 
     <div class="row">
-        <div class="col-sm-6">
+        <div class="col-sm-4">
             <a href="{{ URL::route('pronunciations.index') }}" class="btn
             btn-primary btn-block">View Pronunciations</a>
+            <br>
         </div>
-        <div class="col-sm-6">
+        <div class="col-sm-4">
+            <a href="{{ URL::route('pronunciations.index') }}" class="btn
+            btn-primary btn-block">View Voices</a>
+            <br>
+        </div>
+        <div class="col-sm-4">
             <a href="https://acapela-box.com/AcaBox/index.php"
-               class="btn btn-default btn-block">Acapela Speech Demo</a>
+               class="btn btn-primary btn-block">Acapela Speech Demo</a>
+            <br>
         </div>
     </div>
 
