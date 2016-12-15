cc6dddcf2ede56000b694058aa715772a3802f27
Ben Jenkins <benjenkinsv95@gmail.com>
2016-12-14 22:27:43 -0600

    Adding a getting started page.

--- resources/views/getting_started.blade.php
+++ resources/views/getting_started.blade.php
@@ -7,26 +7,28 @@
     <div class="row container">
         <h2>To listen to a pronunciation</h2>
 
-        <h3>Visit the <a href="{{ URL::route('pronunciations.index') }}">pronunciations page</a> and copy a pronunciation.</h3>
-        <img src="/home/images/example_pronunciation.png" />
+        <h3>Visit the <a href="{{ URL::route('pronunciations.index') }}">pronunciations page</a> and copy a pronunciation</h3>
+        <img class="img-responsive" src="/getting-started/images/example_pronunciation.png" />
         <br>
     </div>
 
     <div class="row container">
-        <h3>Into the <a href="https://acapela-box.com/AcaBox/index.php">Acapela Speech Demo's</a> textbox.</h3>
-        <img src="/home/images/add_pronunciation.png" />
+        <h3>Into the <a href="https://acapela-box.com/AcaBox/index.php">Acapela Speech Demo's</a> textbox</h3>
+        <img class="img-responsive" src="/getting-started/images/add_pronunciation.png" />
         <br>
     </div>
 
     <div class="row container">
-        <h3>Then select the pronunciation's voice.</h3>
-        <img src="/home/images/select_voice.png" />
+        <h3>Then select the pronunciation's voice</h3>
+        <img class="img-responsive hidden-xs" src="/getting-started/images/select_language_and_voice.png" />
+
+        <img class="img-responsive visible-xs" src="/getting-started/images/select_language.png" />
         <br>
-        <br>
+        <img class="img-responsive visible-xs" src="/getting-started/images/select_voice.png" />
     </div>
 
     <div class="row container">
-        <h3>Agree to the terms and conditions and press play.</h3>
+        <h3>Agree to the terms and conditions and press play</h3>
         <p>You should now hear the Acapela voice pronouncing the word you chose.</p>
     </div>
 
